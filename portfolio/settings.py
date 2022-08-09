0c0f09b6737851e1bf5bbc0466a8981b3f6a547a
manthanp1012 <manthanpatil912@gmail.com>
2022-08-10 03:43:49 +0530

    first commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -117,8 +117,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = "portfolio/static"
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    ('assets', "portfolio/static"),
+
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
