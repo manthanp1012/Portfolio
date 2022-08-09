3e8de128cd587cc152965a6b8c78d27e1a6e17c9
manthanp1012 <manthanpatil912@gmail.com>
2022-08-10 01:12:14 +0530

    first commit

---
+++ index/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+    phone = models.EmailField(max_length=30)
+    concern = models.TextField(max_length=30)
---
+++ index/views.py
@@ -0,0 +1,33 @@
+from unicodedata import name
+from django.shortcuts import render
+from index.models import Contact
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def project(request):
+    return render(request, 'project.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        concern = request.POST['concern']
+        # print(name)
+        # print(email)
+        # print(phone)
+        # print(concern)
+        obj = Contact(name=name, email=email,
+                      phone=phone, concern=concern)
+        obj.save()
+    return render(request, 'contact.html')
