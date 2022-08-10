1f19049737288eee814beddafd2550563b0a8884
manthanp1012 <manthanpatil912@gmail.com>
2022-08-10 15:38:41 +0530

    initial commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path, os
+import django_heroku
+
 from pickle import TRUE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,6 +132,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+django_heroku.settings(locals())
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
