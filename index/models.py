e51fbf0296dab30d6fd1ecd4917b3365e8c8dc55
manthanp1012 <manthanpatil912@gmail.com>
2022-08-12 00:38:29 +0530

    settings commit

--- index/models.py
+++ index/models.py
@@ -5,10 +5,10 @@ from django.db import models
 
 
 class Contact(models.Model):
-    name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=30)
-    phone = models.CharField(max_length=30)
-    concern = models.TextField(max_length=30)
+    passname = models.CharField(max_length=30)
+    passemail = models.EmailField(max_length=30)
+    passphone = models.CharField(max_length=30)
+    passconcern = models.TextField(max_length=30)
 
     # name = models.CharField(max_length=30)
     # email = models.EmailField(max_length=30)
