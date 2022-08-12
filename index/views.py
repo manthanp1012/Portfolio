39764893e82d2b61b5b6a6642837bc872e11fa9b
manthanp1012 <manthanpatil912@gmail.com>
2022-08-12 13:48:23 +0530

    Commit

--- index/views.py
+++ index/views.py
@@ -1,6 +1,12 @@
 from unicodedata import name
 from django.shortcuts import render
 from index.models import Contact
+from django.core.mail import EmailMultiAlternatives
+from portfolio import settings
+from email.message import EmailMessage
+import smtplib
+import ssl
+import requests
 
 # Create your views here.
 
@@ -17,21 +23,33 @@ def project(request):
     return render(request, 'project.html')
 
 
+def send_mail_contact(mail):
+    email_subject = 'Concern Submitted Successfully'
+    # html_message = render_to_string('cab/Cab_booked_Student.html')
+    email_body = "Your Concern is being Checked out! We will get back to you soon!"
+    msg = EmailMultiAlternatives(
+        email_subject, email_body, 'patilmanthan51@gmail.com', ['patilmanthan51@gmail.com'])
+    # msg.attach_alternative(html_message, "text/html")
+    msg.send()
+    print("sent success")
+
+
 def contact(request):
-    try:
-        if request.method == 'POST':
-            name = request.POST['name']
-            email = request.POST['email']
-            phone = request.POST['phone']
-            concern = request.POST['concern']
-            # print(name)
-            # print(email)
-            # print(phone)
-            # print(concern)
-            obj = Contact(name=name, email=email,
-                          phone=phone, concern=concern)
-            obj.save()
-            # print(obj.name)
-        return render(request, 'contact.html')
-    except:
-        return render(request, 'home.html')
+    # try:
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        concern = request.POST['concern']
+        # print(name)
+        # print(email)
+        # print(phone)
+        # print(concern)
+        obj = Contact(name=name, email=email,
+                      phone=phone, concern=concern)
+        obj.save()
+        send_mail_contact(email)
+        # print(obj.name)
+    return render(request, 'contact.html')
+    # except:
+    #     return render(request, 'home.html')
