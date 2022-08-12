9eaf4708cb9feb3d154abd732430172294aad009
manthanp1012 <manthanpatil912@gmail.com>
2022-08-12 10:50:47 +0530

    commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-4c9r55g^4s=4a_b85pu@r5w3vkl8fuf4zrforsc-t1iv)tcot)
 
 #DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', 'portfoliomanthan.herokuapp.com']
+ALLOWED_HOSTS = ['portfoliomanthan.herokuapp.com']
 # if DEBUG:
 #     ALLOWED_HOSTS = ['127.0.0.1']
 # export SECRET_KEY = "1bbdbf0c3db28966da01043eaddb01a1af63b888a592d5fc"
