746ba65856cedb02c7130ed1f2e6e4177da860c0
manthanp1012 <manthanpatil912@gmail.com>
2022-08-15 11:34:42 +0530

    mail

--- index/models.py
+++ index/models.py
@@ -9,7 +9,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=30)
     email = models.EmailField(max_length=30)
     phone = models.CharField(max_length=30)
-    concern = models.TextField(max_length=30)
+    concern = models.TextField(max_length=100)
 
     # name = models.CharField(max_length=30)
     # email = models.EmailField(max_length=30)
